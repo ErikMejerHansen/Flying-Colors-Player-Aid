dc4d11600cafd7030e53370047798bc1bc649e78
Erik Mejer Hansen <erik.mejer.hansen@gmail.com>
2012-03-31 21:46:30 +0200

    calling assert_text causes the app view to be scrolled to the bottom so that the base firepower can be seen and therefor asserted

--- features/step_definitions/assert_base_firepower.rb
+++ features/step_definitions/assert_base_firepower.rb
@@ -1,3 +1,4 @@
 Then /^my Base Firepower is "([^"]*)"$/ do |expected_firepower|
+  performAction('assert_text', 'Base firepower:', true)
   performAction('assert_text_in_textview', expected_firepower, 'Caluclated base firepower')
 end
